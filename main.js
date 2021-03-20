6e2b45e4d2bf9d466eaf4e167c6b14c92b268e03
Kyle Cunningham <kyle8david@gmail.com>
2021-03-20 12:52:47 -0600

    user can start car #3

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function Driver(car) {
+function Driver(car, x, y) {
   this.car = document.querySelector('.car');
+  this.x = 0;
+  this.y = 0;
 }
+
+var body = document.querySelector('body');
 
 var game = new Driver();
 
@@ -15,6 +19,28 @@ Driver.prototype.turnCar = function (event) {
   }
 };
 
-document.addEventListener('keydown', function () {
+Driver.prototype.renderGame = function () {
+  game.car.style.left = game.x;
+  game.car.style.top = game.y;
+  body.appendChild(game.car);
+};
+
+Driver.prototype.startCar = function (event) {
+  if (event.code === 'Space') {
+    setInterval(function () {
+      document.body.innerHTML = '';
+      var left = parseInt(game.x || 10);
+      game.x = (left + 3) + 'px';
+      game.renderGame();
+    }, 16);
+  }
+};
+
+document.addEventListener('keydown', function (event) {
   game.turnCar(event);
+  game.startCar(event);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  game.renderGame();
 });
